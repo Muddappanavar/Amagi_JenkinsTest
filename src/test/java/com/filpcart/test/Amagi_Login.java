be385d718a97522d08a867131a3258c536226fbd
ifocus <ifocus.automation@gmail.com>
2023-04-10 16:49:04 +0530

    changes

---
+++ src/test/java/com/filpcart/test/Amagi_Login.java
@@ -0,0 +1,13 @@
+package com.filpcart.test;
+
+import org.testng.annotations.Test;
+
+import freemarker.ext.beans.TemplateAccessible;
+
+public class Amagi_Login {
+
+	@Test
+	public void log() {
+		System.out.println("login");
+	}
+}
